b4bfded092b786d95777c720547b00e5a1e6177c
daniil3006 <daniil.stank@gmail.com>
2024-06-08 12:48:00 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void fun(int* arr1, int* arr2, int n){
+    __asm__(
+            ".intel_syntax noprefix;"
+            "while_start:;"
+            "cmp    ecx, %[n];"
+            "je     while_end;"
+            "mov    r8d, [rax + rcx * 4];"
+            "mov    [rbx + rcx * 4], r8d;"
+            "inc    ecx;"
+            "jmp    while_start;"
+            "while_end:;"
+            ".att_syntax;"
+            :
+            :
+            [n] "S" (n), "a"(arr1), "b"(arr2), "c"(0)
+            );
+}
+
+int main() {
+    int arr1[2] = {1, 2};
+    int arr2[2];
+    fun(arr1, arr2, 2);
+    return 0;
+}
